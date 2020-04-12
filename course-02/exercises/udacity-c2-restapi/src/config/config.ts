0d47b041126c251854f0635949f9a85d641832cf
Moemad-711 <mohammedemad4@gmail.com>
2020-04-12 08:59:17 +0200

    Finished excercises

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramemaddev",
+    "password": "Mo11797Mymema44",
+    "database": "udagramemaddev",
+    "host": "udagramemaddev.ccmnex80iqv7.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,41 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id',async(req: Request, res: Response) => {
+    const {id} = req.params;
+    const item = await FeedItem.findByPk(id);
+    if(!item)
+    {
+        return res.status(404).send("item with this id is not found");
+    }
+
+    if(item.url)
+        item.url = AWS.getGetSignedUrl(item.url);
+
+    return res.status(200).send(item);
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        console.log(req.body);
+        console.log(req.params);
+        const {id} = req.params;
+        const item = await FeedItem.findByPk(id)
+        if(!item)
+        {
+            return res.status(404).send("item with this id is not found");
+        }
+    
+        if(item.url)
+        {    
+            item.url = AWS.getGetSignedUrl(item.url);
+        }
+        
+        item.update(req.body);
+        return res.status(200).send(item);
 });
 
 
