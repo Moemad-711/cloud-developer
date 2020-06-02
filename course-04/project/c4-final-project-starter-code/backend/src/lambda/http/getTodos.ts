4b6a94876f82aab9ed69784f14eba5584599bd59
Moemad-711 <mohammedemad4@gmail.com>
2020-06-02 18:24:50 +0200

    Update getTodos.ts

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,39 @@
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { parseUserId } from '../../auth/utils'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+
+const docClient: DocumentClient = new  AWS.DynamoDB.DocumentClient()
+const TODOTable = process.env.TODO_TABLE
+const bucketName = process.env.attachment_S3_BUCKET
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const userId = parseUserId(jwtToken)
+
+  const result = await docClient.query({
+    TableName: TODOTable,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    },
+    ScanIndexForward: false
+  }).promise()
+
+
+  return{
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: result.Items
+    })
+  }
 }
