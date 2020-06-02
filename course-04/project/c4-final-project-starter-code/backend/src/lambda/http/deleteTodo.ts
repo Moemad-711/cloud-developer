7ce314330d351d1159f40098ecd94a17d8759ea1
Moemad-711 <mohammedemad4@gmail.com>
2020-06-02 19:17:42 +0200

    Update deleteTodo.ts

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,46 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { parseUserId } from '../../auth/utils'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import * as AWS from 'aws-sdk'
+
+const docClient: DocumentClient = new  AWS.DynamoDB.DocumentClient()
+const TODOTable = process.env.TODO_TABLE
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   // TODO: Remove a TODO item by id
-  return undefined
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const userId = parseUserId(jwtToken)  
+
+  await docClient.delete({
+    TableName: TODOTable,
+    ConditionExpression:'userId = :userId AND todoId = :todoId',
+    ExpressionAttributeValues:{
+      ':userId': userId,
+      ':todoId': todoId
+    }, 
+    Key:{
+      userId: userId,
+      todoId: todoId
+    }
+  })
+
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      message: 'Item Deleted'
+      
+    })
+  }
 }
