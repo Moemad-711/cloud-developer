340fee87ed6710ed033f2a461c0ba4c0d767bf92
Moemad-711 <mohammedemad4@gmail.com>
2020-06-03 06:00:36 +0200

    Update generateUploadUrl.ts

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,25 +1,43 @@
 import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import * as AWS  from 'aws-sdk'
 
-const s3 = new AWS.S3({
+
+const XAWS = AWSXRay.captureAWS(AWS)
+const s3 = new XAWS.S3({
   signatureVersion: 'v4'
 })
 
-const bucketName = process.env.IMAGES_S3_BUCKET
+const bucketName = process.env.ATTACH_S3_BUCKET
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+  console.log('Caller event', event)
+
+  if(!todoId)
+  {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: 'todoId Parameter is missing'
+    }
+  }
+
   const uploadUrl = getUploadUrl(todoId)
 
   return {
     statusCode: 201,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
     },
     body: JSON.stringify({
       uploadUrl: uploadUrl
